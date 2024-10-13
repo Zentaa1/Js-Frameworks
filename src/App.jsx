61cb907009e376cd5f7039c0b1cb30320657460f
Zentaa1 <xprozenta@gmail.com>
2024-10-13 15:42:22 +0200

    Fixed the build

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './pages/Contact';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import CheckoutSuccess from './pages/CheckoutSuccess';
-import { CartProvider } from './components/functions/CartContext.jsx';
+import { CartProvider } from './components/functions/CartContext';
 
 const App = () => {
   return (
